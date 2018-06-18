12a283e7e7d008c3a9c3d7552f9f29a659cf8123
Erik Withak <ErikAanestad@gmail.com>
2018-06-18 12:50:37 -0700

    done did stuffs

--- scripts/drop-tables.js
+++ scripts/drop-tables.js
@@ -2,7 +2,10 @@ require('dotenv').config();
 const client = require('../db-client');
 
 client.query(`
-  DROP TABLE IF EXISTS restaurants;
+  DROP TABLE IF EXISTS foods;
+  DROP TABLE IF EXISTS types;
+  DROP TABLE IF EXISTS months;
+  DROP TABLE IF EXISTS seasons;
 `)
   .then(
     () => console.log('drop tables complete'),
